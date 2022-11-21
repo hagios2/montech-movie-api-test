660245d2ecb1fdb261dc37ca75c29de4307736c5
Oteng <hagioswilson@gmail.com>
2022-11-21 10:32:30 +0000

    made changes

--- src/services/Auth/AuthService.js
+++ src/services/Auth/AuthService.js
@@ -9,18 +9,19 @@ export const logInUser = async (credentials) => {
     throw new Error('User not found')
   }
   await compareHash(user?.password, password)
-  const token = signToken(user.toJSON())
+  const token = signToken(user.doc.toJSON())
+  console.log(user.toJSON(), user.doc.toJSON())
   user = await User.findByIdAndUpdate(
     user?._id,
     {
-      token: 'KAB',
+      token,
       last_login: new Date().toISOString(),
     },
     { new: true }
   )
-  .select('_id name email createdAt token last_login')
-  
-  return {user, token}
+    .select('_id name email createdAt token last_login')
+
+  return { user, token }
 }
 
 export const logOutUser = async (userId) => {
